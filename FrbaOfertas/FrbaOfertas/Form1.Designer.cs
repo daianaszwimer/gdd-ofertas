02ab7cf28939fa4efcbecfa74450cf9084949212
Diego Peccia <diegoignaciopeccia@gmail.com>
2019-09-30 16:51:09 -0300

    Primer approach Log In sin codificacion en contraseña

--- FrbaOfertas/FrbaOfertas/Form1.Designer.cs
+++ FrbaOfertas/FrbaOfertas/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(55, 46);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 20);
+            this.username.TabIndex = 0;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(55, 85);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 1;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(55, 126);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 2;
+            this.login.Text = "Log In";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button login;
     }
 }
 
--- FrbaOfertas/FrbaOfertas/Form1.cs
+++ FrbaOfertas/FrbaOfertas/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,43 @@ namespace FrbaOfertas
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection dbOfertas;
+
         public Form1()
         {
             InitializeComponent();
+            conectarseABaseDeDatosOfertas();
+        }
+
+        private void conectarseABaseDeDatosOfertas() 
+        {
+            try
+            {
+                string dbOfertasConnectionString = ConfigurationManager.ConnectionStrings["dbOfertas"].ConnectionString;
+                dbOfertas = new SqlConnection(dbOfertasConnectionString);
+                dbOfertas.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo conectar a la base de datos");
+            }
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            SqlCommand chequearLogIn = new SqlCommand("SELECT username, pass FROM pruebaLogin WHERE username='" + username.Text + "' AND pass='" + password.Text + "'", dbOfertas);
+            
+            SqlDataReader dataReader = chequearLogIn.ExecuteReader();
+            if (dataReader.Read())
+            {
+                MessageBox.Show("Log In exitoso");
+            }
+            else
+            {
+                MessageBox.Show("Datos incorrectos");
+            }
+            dataReader.Close();
         }
     }
 }
