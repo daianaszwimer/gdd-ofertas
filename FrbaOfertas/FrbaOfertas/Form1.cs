d5efadd6e505a14f170d646ecc5abd8f91789d22
Diego Peccia <diegoignaciopeccia@gmail.com>
2019-10-01 23:50:10 -0300

    Encriptacion SHA-256 para la contrasenia

--- FrbaOfertas/FrbaOfertas/Form1.cs
+++ FrbaOfertas/FrbaOfertas/Form1.cs
@@ -9,6 +9,7 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace FrbaOfertas
 {
@@ -39,7 +40,9 @@ namespace FrbaOfertas
 
         private void login_Click(object sender, EventArgs e)
         {
-            SqlCommand chequearLogIn = new SqlCommand("SELECT username, pass FROM pruebaLogin WHERE username='" + username.Text + "' AND pass='" + password.Text + "'", dbOfertas);
+            // Con una tabla de prueba "usuario" que tiene username: admin 
+            // y pass: w23e encriptada como "e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7"
+            SqlCommand chequearLogIn = new SqlCommand("SELECT username, pass FROM usuario WHERE username='" + username.Text + "' AND pass='" + this.SHA256Encrypt(password.Text) + "'", dbOfertas);
             
             SqlDataReader dataReader = chequearLogIn.ExecuteReader();
             if (dataReader.Read())
@@ -51,6 +54,21 @@ namespace FrbaOfertas
                 MessageBox.Show("Datos incorrectos");
             }
             dataReader.Close();
+        }
+
+        public string SHA256Encrypt(string input)
+        {
+            SHA256CryptoServiceProvider provider = new SHA256CryptoServiceProvider();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashedBytes = provider.ComputeHash(inputBytes);
+
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < hashedBytes.Length; i++)
+                output.Append(hashedBytes[i].ToString("x2").ToLower());
+
+            return output.ToString();
         }
     }
 }
