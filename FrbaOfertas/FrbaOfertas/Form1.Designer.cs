a2432593cc6a6b5b0b98b1e497ab860d4200f5fd
mposru <mposru@est.frba.utn.edu.ar>
2019-10-14 21:07:17 -0300

    Formato pantalla login

--- FrbaOfertas/FrbaOfertas/Form1.Designer.cs
+++ FrbaOfertas/FrbaOfertas/Form1.Designer.cs
@@ -31,42 +31,122 @@
             this.username = new System.Windows.Forms.TextBox();
             this.password = new System.Windows.Forms.TextBox();
             this.login = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(55, 46);
+            this.username.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.username.Font = new System.Drawing.Font("Lucida Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.username.Location = new System.Drawing.Point(94, 45);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(100, 20);
+            this.username.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.username.Size = new System.Drawing.Size(160, 20);
             this.username.TabIndex = 0;
+            this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(55, 85);
+            this.password.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.password.Location = new System.Drawing.Point(94, 103);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.PasswordChar = '*';
+            this.password.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.password.Size = new System.Drawing.Size(160, 20);
             this.password.TabIndex = 1;
+            this.password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(55, 126);
+            this.login.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.login.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.login.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.login.Font = new System.Drawing.Font("Lucida Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.login.Location = new System.Drawing.Point(94, 149);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.login.Size = new System.Drawing.Size(160, 33);
             this.login.TabIndex = 2;
-            this.login.Text = "Log In";
-            this.login.UseVisualStyleBackColor = true;
+            this.login.Text = "LOGIN";
+            this.login.UseVisualStyleBackColor = false;
             this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.username);
+            this.panel1.Controls.Add(this.password);
+            this.panel1.Controls.Add(this.login);
+            this.panel1.Location = new System.Drawing.Point(24, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(347, 226);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(92, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FRBA - Ofertas";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(136, 27);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ussername";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(140, 82);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.login);
-            this.Controls.Add(this.password);
-            this.Controls.Add(this.username);
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(395, 307);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +157,10 @@
         private System.Windows.Forms.TextBox username;
         private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
