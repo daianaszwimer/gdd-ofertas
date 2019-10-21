907c2521239408a94510de34a7db0e4d84092ffe
mposru <mposru@est.frba.utn.edu.ar>
2019-10-21 09:53:02 -0300

    Control intentos fallidos login

--- FrbaOfertas/FrbaOfertas/Form1.cs
+++ FrbaOfertas/FrbaOfertas/Form1.cs
@@ -23,11 +23,9 @@ namespace FrbaOfertas
         }
 
         private void login_Click(object sender, EventArgs e)
-        {
-            this.Hide();
+        {          
             AbmRol.Form1 abmRol = new AbmRol.Form1();
-            abmRol.Show();
-            /*
+            
             // Con una tabla de prueba "usuario" que tiene username: admin 
             // y pass: w23e encriptada como "e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7"
             SqlCommand chequearLogIn = new SqlCommand(string.Format("SELECT username, pass FROM usuario WHERE username='{0}' AND pass='{1}'", username.Text, this.SHA256Encrypt(password.Text)), dbOfertas);
@@ -35,13 +33,32 @@ namespace FrbaOfertas
             SqlDataReader dataReader = chequearLogIn.ExecuteReader();
             if (dataReader.Read())
             {
+                dataReader.Close();
                 MessageBox.Show("Log In exitoso");
+                SqlCommand loginCorrecto = new SqlCommand(string.Format("UPDATE dbo.usuario SET intentos_fallidos_login = 0 WHERE username='" + username.Text + "'"), dbOfertas); 
+                SqlDataReader dataReader1 = loginCorrecto.ExecuteReader();
+                dataReader1.Close();
+                this.Hide();
+                abmRol.Show();
             }
             else
             {
-                MessageBox.Show("Datos incorrectos");
+                try
+                {
+                    dataReader.Close();
+                    SqlCommand loginIncorrecto = new SqlCommand(string.Format("UPDATE dbo.usuario SET intentos_fallidos_login = intentos_fallidos_login+1 WHERE username='" + username.Text + "'"), dbOfertas); 
+                    SqlDataReader dataReader2 = loginIncorrecto.ExecuteReader();
+                    MessageBox.Show("Datos incorrectos");
+                    this.Show();
+                    dataReader2.Close();
+                }
+                catch (System.Data.SqlClient.SqlException ex)
+                {
+                    this.Hide();
+                    MessageBox.Show("Limite login");
+
+                }
             }
-            dataReader.Close();*/
         }
 
         public string SHA256Encrypt(string input)
