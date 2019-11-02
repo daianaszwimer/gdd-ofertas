cb38054edecc452f040e73913c27ef2752553d6d
Diego Peccia <diegoignaciopeccia@gmail.com>
2019-11-02 16:22:58 -0300

    Agrego a la query roles y funcionalidades

--- SQLQueryInicio.sql
+++ SQLQueryInicio.sql
@@ -1,9 +1,10 @@
 use GD2C2019
 
-drop table	dbo.funcionalidadxrol,
+drop table	dbo.rolxusuario,
+			dbo.funcionalidadxrol,
 			dbo.usuario,
 			dbo.funcionalidad,
-			dbo.rol
+			dbo.rol,
 
 create table usuario(
 	username varchar(50) PRIMARY KEY NOT NULL,
@@ -24,13 +25,25 @@ create table funcionalidadxrol(
 	rol_id int not null REFERENCES rol,
 	funcionalidad_id int not null REFERENCES funcionalidad)
 
+create table rolxusuario(
+	username varchar(50) not null REFERENCES usuario,
+	rol_id int not null REFERENCES rol)
+
 insert into usuario (username, pass)
 	values ('admin','e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7'),
 	('maru','aaa')
-
-
 
 insert into funcionalidad (descripcion)
 	values ('ABM Cliente'),('ABM Proveedor'),('Carga de credito'),
 			('Alta de oferta'),('Compra de oferta'),('Baja de oferta'),
 			('Facturacion'),('Estadisticas')
+
+insert into rol (rol_nombre)
+	values ('Administrador General'),('Cliente')
+
+insert into funcionalidadxrol (rol_id, funcionalidad_id)
+	values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),
+		(2,3),(2,5),(2,7)
+
+insert into rolxusuario (username, rol_id)
+	VALUES ('admin', 1), ('maru',2)
