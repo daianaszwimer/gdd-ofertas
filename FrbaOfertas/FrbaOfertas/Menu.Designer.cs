53ad5ac4391997e00cc5fe14f6762f6b69cfa335
Diego Peccia <diegoignaciopeccia@gmail.com>
2019-11-03 11:58:23 -0300

    Primer approach Menu

---
+++ FrbaOfertas/FrbaOfertas/Menu.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace FrbaOfertas
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel.AutoScroll = true;
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Location = new System.Drawing.Point(32, 60);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.Size = new System.Drawing.Size(259, 236);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 354);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+
+
+    }
+}
---
+++ FrbaOfertas/FrbaOfertas/Menu.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas
+{
+    public partial class Menu : Utils
+    {
+        public Menu()
+        {
+            String usuario = "admin";
+            InitializeComponent();
+            conectarseABaseDeDatosOfertas();
+            List<String> funcionalidades = obtenerFuncionalidadesSegunUsuario(usuario);
+            mostrarListadoDeFuncionalidades(funcionalidades);
+        }
+
+        private List<String> obtenerFuncionalidadesSegunUsuario(String usuario)
+        {
+            List<String> funcionalidades = new List<string>();
+            SqlCommand seleccionarFuncionalidades = 
+                new SqlCommand("SELECT f.descripcion FROM rol r " +
+	                                "JOIN funcionalidadxrol fr ON fr.rol_id = r.rol_id " +
+	                                "JOIN funcionalidad f ON f.id = fr.funcionalidad_id " +
+	                                "JOIN rolxusuario ru ON ru.rol_id = r.rol_id " +
+	                                "JOIN usuario u ON u.username = ru.username " +
+	                                "WHERE u.username ='" + usuario + "'", dbOfertas);
+            SqlDataReader dataReader = seleccionarFuncionalidades.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                string funcionalidad = dataReader.GetValue(0).ToString();
+                funcionalidades.Add(funcionalidad);
+            }
+            dataReader.Close();
+            return funcionalidades;
+        }
+
+        private void mostrarListadoDeFuncionalidades(List<String> funcionalidades) 
+        {
+            int columnas = 2;
+            int filas = (int)Math.Ceiling((double)(funcionalidades.Count / 2));
+            tableLayoutPanel.ColumnCount = columnas;
+            tableLayoutPanel.RowCount = filas;
+            tableLayoutPanel.Dock = DockStyle.Fill;
+
+            foreach (var funcionalidad in funcionalidades)
+            {
+                /*var rectangulo = new Rectangle();
+                rectangulo.Width = */
+                var buttonFuncionalidad = new Button();
+                buttonFuncionalidad.Text = funcionalidad;
+                agregarEventoOnClick(buttonFuncionalidad);
+                buttonFuncionalidad.Dock = DockStyle.Fill;
+                buttonFuncionalidad.Height = 100;
+                tableLayoutPanel.Controls.Add(buttonFuncionalidad);
+            }
+        }
+
+        private void agregarEventoOnClick(Button buttonFuncionalidad)
+        {
+            switch (buttonFuncionalidad.Text)
+            {
+                case "ABM Rol": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new AbmRol.Form1()).Show(); }; break;
+                case "ABM Cliente": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new AbmCliente.Form1()).Show(); }; break;
+                case "ABM Proveedor": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new AbmProveedor.Form1()).Show(); }; break;
+                case "Carga de credito": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new CragaCredito.Form1()).Show(); }; break;
+                case "Confeccion y publicacion de Ofertas": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new CrearOferta.Form1()).Show(); }; break;
+                case "Compra de oferta": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new ComprarOferta.Form1()).Show(); }; break;
+                case "Entrega/Consumo de oferta": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new AbmCliente.Form1()).Show(); }; break;
+                case "Facturacion a Proveedor": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new Facturar.Form1()).Show(); }; break;
+                case "Listado Estadistico": buttonFuncionalidad.Click += (object sender, EventArgs e) => { (new ListadoEstadistico.Form1()).Show(); }; break;
+            }
+            
+        }
+    }
+}
