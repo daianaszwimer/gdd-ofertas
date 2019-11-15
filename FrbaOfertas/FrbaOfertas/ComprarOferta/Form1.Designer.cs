cbfeeccf81f98a527011cefa664614cd4a1b79b5
Diego Peccia <diegoignaciopeccia@gmail.com>
2019-11-15 17:18:58 -0300

    Cosa de mari

--- FrbaOfertas/FrbaOfertas/ComprarOferta/Form1.Designer.cs
+++ FrbaOfertas/FrbaOfertas/ComprarOferta/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.tablaDeResultados = new System.Windows.Forms.DataGridView();
             this.buscar = new System.Windows.Forms.Button();
+            this.comprar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.tablaDeResultados)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,12 +44,12 @@
             this.tablaDeResultados.Location = new System.Drawing.Point(29, 102);
             this.tablaDeResultados.Name = "tablaDeResultados";
             this.tablaDeResultados.ReadOnly = true;
-            this.tablaDeResultados.Size = new System.Drawing.Size(419, 191);
+            this.tablaDeResultados.Size = new System.Drawing.Size(419, 151);
             this.tablaDeResultados.TabIndex = 1;
             // 
             // buscar
             // 
-            this.buscar.Location = new System.Drawing.Point(373, 73);
+            this.buscar.Location = new System.Drawing.Point(29, 73);
             this.buscar.Name = "buscar";
             this.buscar.Size = new System.Drawing.Size(75, 23);
             this.buscar.TabIndex = 3;
@@ -55,17 +57,44 @@
             this.buscar.UseVisualStyleBackColor = true;
             this.buscar.Click += new System.EventHandler(this.buscar_Click);
             // 
+            // comprar
+            // 
+            this.comprar.Location = new System.Drawing.Point(373, 73);
+            this.comprar.Name = "comprar";
+            this.comprar.Size = new System.Drawing.Size(75, 23);
+            this.comprar.TabIndex = 4;
+            this.comprar.Text = "Comprar";
+            this.comprar.UseVisualStyleBackColor = true;
+            this.comprar.Click += new System.EventHandler(this.comprar_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(25, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ofertas Disponibles";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 265);
+            this.ClientSize = new System.Drawing.Size(494, 297);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comprar);
             this.Controls.Add(this.buscar);
             this.Controls.Add(this.tablaDeResultados);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Controls.SetChildIndex(this.tablaDeResultados, 0);
             this.Controls.SetChildIndex(this.buscar, 0);
+            this.Controls.SetChildIndex(this.comprar, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
             ((System.ComponentModel.ISupportInitialize)(this.tablaDeResultados)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -76,5 +105,7 @@
 
         private System.Windows.Forms.DataGridView tablaDeResultados;
         private System.Windows.Forms.Button buscar;
+        private System.Windows.Forms.Button comprar;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FrbaOfertas/FrbaOfertas/ComprarOferta/Form1.cs
+++ FrbaOfertas/FrbaOfertas/ComprarOferta/Form1.cs
@@ -18,19 +18,48 @@ namespace FrbaOfertas.ComprarOferta
         public Form1()
         {
             InitializeComponent();
+            comprar.Visible = false;
             tablaDeResultados.SelectionChanged += tablaDeResultados_SelectionChanged;
         }
 
         private void tablaDeResultados_SelectionChanged(object sender, EventArgs e)
         {
+            if (tablaDeResultados.SelectedRows.Count == 0)
+            {
+                comprar.Visible = false;
+            }
+            else
+            {
+                comprar.Visible = true;
+            }
         }
 
         private void buscar_Click(object sender, EventArgs e)
         {
             ofertasDataSet.Clear();
-            SqlDataAdapter ofertasDataAdapter = new SqlDataAdapter(string.Format("SELECT oferta_id, oferta_descripcion, oferta_precio_lista FROM Oferta WHERE oferta_fecha_venc >= '{0}'", Helper.obtenerFechaActual().ToString("yyyy-MM-dd HH:mm:ss.fff")), Helper.dbOfertas);
+            SqlDataAdapter ofertasDataAdapter = new SqlDataAdapter(string.Format("SELECT oferta_id, oferta_descripcion, oferta_precio_lista, oferta_cantidad FROM Oferta WHERE oferta_fecha_venc >= '{0}' AND oferta_cantidad > 0", Helper.obtenerFechaActual().ToString("yyyy-MM-dd HH:mm:ss.fff")), Helper.dbOfertas);
             ofertasDataAdapter.Fill(ofertasDataSet);
             tablaDeResultados.DataSource = ofertasDataSet.Tables[0];
+        }
+
+        private void comprar_Click(object sender, EventArgs e)
+        {
+            object[] oferta = Helper.obtenerValoresFilaSeleccionada(tablaDeResultados);
+            string id = oferta[0].ToString();
+
+            //TODO: VALIDAR SALDO SUFICIENTE
+            //TODO: CANTIDAD MAX USUARIO
+            SqlCommand restarOferta = new SqlCommand(string.Format("UPDATE Oferta SET oferta_cantidad = oferta_cantidad-1 WHERE oferta_id="+ id), Helper.dbOfertas);
+            SqlDataReader dataReader = Helper.realizarConsultaSQL(restarOferta);
+            if (dataReader != null)
+            {
+                MessageBox.Show("DATOS DE COMPRA", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                ofertasDataSet.Clear();
+                dataReader.Close();
+            }
+            dataReader.Close();
+
         }
     }
 }
