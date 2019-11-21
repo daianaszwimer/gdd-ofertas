b1745d5a1fa9f39a4dab9708691e7481b02e022b
mposru <mposru@est.frba.utn.edu.ar>
2019-11-20 21:55:56 -0300

    combo box con cupones ponele

--- FrbaOfertas/FrbaOfertas/EntregaDeOferta/Form1.cs
+++ FrbaOfertas/FrbaOfertas/EntregaDeOferta/Form1.cs
@@ -14,11 +14,13 @@ namespace FrbaOfertas.EntregaDeOferta
     public partial class Form1 : BarraDeOpciones
     {
         string idProveedor;
+        DataSet cuponesDataSet = new DataSet();
 
         public Form1()
         {
             InitializeComponent();
-            //codigoCupon.DataSource = obtenerClientesPosibles();
+            obtenerIdProveedor();
+            obtenerCuponesPosibles();
         }
 
         private void bajaCupon_Click(object sender, EventArgs e)
@@ -29,49 +31,36 @@ namespace FrbaOfertas.EntregaDeOferta
                 DateTime myDateTime = Helper.obtenerFechaActual();
                 string sqlFormattedDate = myDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
 
-                SqlCommand obtenerIdProveedor = new SqlCommand( "SELECT proveedor_id FROM NO_LO_TESTEAMOS_NI_UN_POCO.Proveedor " +
-                     "JOIN NO_LO_TESTEAMOS_NI_UN_POCO.Usuario ON usuario_username = proveedor_id_usuario WHERE usuario_username='" + Helper.usuarioActual + "'", Helper.dbOfertas);
-                SqlDataReader dataReaderProveedor = Helper.realizarConsultaSQL(obtenerIdProveedor);
+                SqlCommand obtenerIdCliente = new SqlCommand("SELECT cliente_id FROM NO_LO_TESTEAMOS_NI_UN_POCO.Cliente " +
+                    "WHERE cliente_dni= " + clienteDni.Text, Helper.dbOfertas);
+                SqlDataReader dataReaderCliente = Helper.realizarConsultaSQL(obtenerIdCliente);
 
-                if (dataReaderProveedor != null)
+                if (dataReaderCliente != null)
                 {
-                    if (dataReaderProveedor.Read())
+                    if (dataReaderCliente.Read())
                     {
-                        idProveedor = dataReaderProveedor.GetValue(0).ToString();
-                        dataReaderProveedor.Close();
-
-                        SqlCommand obtenerIdCliente = new SqlCommand("SELECT cliente_id FROM NO_LO_TESTEAMOS_NI_UN_POCO.Cliente " +
-                            "WHERE cliente_dni= " + clienteDni.Text, Helper.dbOfertas);
-                        SqlDataReader dataReaderCliente = Helper.realizarConsultaSQL(obtenerIdCliente);
-
-                        if (dataReaderCliente != null) 
+                        string idCliente = dataReaderCliente.GetValue(0).ToString();
+                        dataReaderCliente.Close();
+                        try
                         {
-                            if (dataReaderCliente.Read())
+                            using (SqlCommand cmd = Helper.dbOfertas.CreateCommand())
                             {
-                                string idCliente = dataReaderCliente.GetValue(0).ToString();
-                                dataReaderCliente.Close();
-                                try
-                                {
-                                    using (SqlCommand cmd = Helper.dbOfertas.CreateCommand())
-                                    {
-                                        cmd.CommandText = "NO_LO_TESTEAMOS_NI_UN_POCO.proveedor_entrega_oferta";
-                                        cmd.CommandType = CommandType.StoredProcedure;
-                                        cmd.Parameters.AddWithValue("fecha_consumo", sqlFormattedDate);
-                                        cmd.Parameters.AddWithValue("id_proveedor", idProveedor);
-                                        cmd.Parameters.AddWithValue("id_cliente", clienteDni.Text);
-                                        cmd.Parameters.AddWithValue("codigo_cup", codigoCupon.Text);
+                                cmd.CommandText = "NO_LO_TESTEAMOS_NI_UN_POCO.proveedor_entrega_oferta";
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Parameters.AddWithValue("fecha_consumo", sqlFormattedDate);
+                                cmd.Parameters.AddWithValue("id_proveedor", idProveedor);
+                                cmd.Parameters.AddWithValue("id_cliente", idCliente);
+                                cmd.Parameters.AddWithValue("codigo_cup", codigoCupon.Text);
 
-                                        cmd.ExecuteNonQuery();
-                                    }
-                                }
-                                catch (SqlException ex)
-                                {
-                                    MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                }
+                                cmd.ExecuteNonQuery();
                             }
-
+                        }
+                        catch (SqlException ex)
+                        {
+                            MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
                     }
+
                 }
             }
             else
@@ -80,6 +69,26 @@ namespace FrbaOfertas.EntregaDeOferta
             }
         }
 
+        private void obtenerIdProveedor()
+        {
+            SqlCommand obtenerIdProveedor = new SqlCommand("SELECT proveedor_id FROM NO_LO_TESTEAMOS_NI_UN_POCO.Proveedor JOIN NO_LO_TESTEAMOS_NI_UN_POCO.Usuario ON usuario_username = proveedor_id_usuario WHERE usuario_username='" + Helper.usuarioActual + "'", Helper.dbOfertas);
+            SqlDataReader dataReaderProveedor = Helper.realizarConsultaSQL(obtenerIdProveedor);
+            if (dataReaderProveedor != null)
+            {
+                if (dataReaderProveedor.Read())
+                {
+                    idProveedor = dataReaderProveedor.GetValue(0).ToString();
+                    dataReaderProveedor.Close();
+                }
+            }
+        }
+
+        private void obtenerCuponesPosibles()
+        {
+            SqlDataAdapter cuponesDataAdapter = new SqlDataAdapter(string.Format("SELECT * FROM NO_LO_TESTEAMOS_NI_UN_POCO.obtener_codigos_cupones({0})", idProveedor), Helper.dbOfertas);
+            cuponesDataAdapter.Fill(cuponesDataSet);
+            codigoCupon.DataSource = cuponesDataSet.Tables[0];
+       }
 
 
     }
