f6e3b6f8e19039db9ac1cfeebfd62ab422a3acb7
Daiana Szwimer <daiana.szwimer@prosoftwarefactory.com.ar>
2019-11-22 20:22:12 -0300

    genero cupones de ofertas que no fueron retiradas

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -514,9 +514,8 @@ update CTE_Codigos set compra_oferta_codigo = concat(SUBSTRING(compra_oferta_cod
  where num_col > 1 
 
 
--- no tenemos fecha de vencimiento del cupon, ponemos la misma que la que fue consumido, total ya fue consumido -> chequear que no hayan compras no consumidas HAY COMPRAS NO CONSUMIDAS
 insert into [NO_LO_TESTEAMOS_NI_UN_POCO].Cupon(cupon_fecha_venc, cupon_fecha_consumo, cupon_id_compra_oferta, cupon_id_cliente, cupon_codigo)
-  select distinct  m.Oferta_Entregado_Fecha, m.Oferta_Entregado_Fecha,
+  select distinct  m.Oferta_Fecha_Venc, m.Oferta_Entregado_Fecha,
   co.compra_oferta_id, 
   c.cliente_id, co.compra_oferta_codigo
   from [gd_esquema].Maestra m 
@@ -531,6 +530,35 @@ insert into [NO_LO_TESTEAMOS_NI_UN_POCO].Cupon(cupon_fecha_venc, cupon_fecha_con
 	co.compra_oferta_id_oferta = o.oferta_id and co.compra_oferta_fecha = m.Oferta_Fecha_Compra
 	and co.compra_oferta_id_cliente = c.cliente_id
   where m.Oferta_Entregado_Fecha is not null
+
+-- inserto cupones que no fueron retirados todavia
+insert into [NO_LO_TESTEAMOS_NI_UN_POCO].Cupon(cupon_fecha_venc, cupon_id_compra_oferta, cupon_codigo)
+	select distinct '2020-02-02', c.compra_oferta_id, c.compra_oferta_codigo from NO_LO_TESTEAMOS_NI_UN_POCO.Compra_Oferta c
+	left join gd_esquema.Maestra m on m.Oferta_Fecha_Compra = c.compra_oferta_fecha
+	left join NO_LO_TESTEAMOS_NI_UN_POCO.Cliente on cliente_dni = m.Cli_Dni and c.compra_oferta_id_cliente = cliente_id
+	join NO_LO_TESTEAMOS_NI_UN_POCO.Proveedor on proveedor_cuit = m.Provee_CUIT and proveedor_razon_social = m.Provee_RS
+	left join NO_LO_TESTEAMOS_NI_UN_POCO.Oferta o on o.oferta_cantidad = m.Oferta_Cantidad 
+	and o.oferta_precio = m.Oferta_Precio and o.oferta_precio_lista = m.Oferta_Precio_Ficticio
+	and proveedor_id = o.oferta_id_proveedor and o.oferta_fecha_publicacion = m.Oferta_Fecha
+	and o.oferta_fecha_venc = m.Oferta_Fecha_Venc and c.compra_oferta_id_oferta = o.oferta_id
+	and o.oferta_descripcion = m.Oferta_Descripcion
+	left join NO_LO_TESTEAMOS_NI_UN_POCO.Cupon on c.compra_oferta_id = cupon_id_compra_oferta
+	where cupon_id_compra_oferta is null
+
+-- como es muy costoso hacerlo de una, inserto y despues hago update
+-- sé que todos los cupones con fecha de vencimiento y cliente en null no fueron retirados y tienen mal puesta la fecha de vencimiento, lo arreglo
+update NO_LO_TESTEAMOS_NI_UN_POCO.Cupon SET NO_LO_TESTEAMOS_NI_UN_POCO.Cupon.cupon_fecha_venc = 
+	m.Oferta_Fecha_Venc
+  from [gd_esquema].Maestra m 
+  left join [NO_LO_TESTEAMOS_NI_UN_POCO].Proveedor p on p.proveedor_cuit = m.Provee_CUIT
+  left join NO_LO_TESTEAMOS_NI_UN_POCO.Oferta o on o.oferta_descripcion = m.Oferta_Descripcion and
+  o.oferta_fecha_publicacion = m.Oferta_Fecha and o.oferta_fecha_venc = m.Oferta_Fecha_Venc and
+  o.oferta_precio = m.Oferta_Precio and o.oferta_precio_lista = m.Oferta_Precio_Ficticio and
+  o.oferta_cantidad = m.Oferta_Cantidad and o.oferta_id_proveedor = p.proveedor_id
+  left join [NO_LO_TESTEAMOS_NI_UN_POCO].Compra_Oferta co on 
+	co.compra_oferta_id_oferta = o.oferta_id and co.compra_oferta_fecha = m.Oferta_Fecha_Compra
+ where NO_LO_TESTEAMOS_NI_UN_POCO.Cupon.cupon_fecha_consumo is null and NO_LO_TESTEAMOS_NI_UN_POCO.Cupon.cupon_id_cliente is null
+ and co.compra_oferta_id = Cupon.cupon_id_compra_oferta
 
 SET IDENTITY_INSERT [NO_LO_TESTEAMOS_NI_UN_POCO].Factura ON
 
